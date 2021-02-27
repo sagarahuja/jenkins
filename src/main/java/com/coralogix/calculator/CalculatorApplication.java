619d1e8c31d9e3f47bd670c995cfec02b467821a
Sagar Ahuja <sagarahuja25@gmail.com>
2021-02-27 22:47:11 +0530

    system out print ln

--- src/main/java/com/coralogix/calculator/CalculatorApplication.java
+++ src/main/java/com/coralogix/calculator/CalculatorApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CalculatorApplication {
-
 	public static void main(String[] args) {
+		System.out.println("Testing Spring boot");
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
 

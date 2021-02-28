8a6ef0135cae7e8fdd3aab303bbac3ce3023769b
Sagar Ahuja <sagarahuja25@gmail.com>
2021-02-28 14:06:06 +0530

    updated main app java file

--- src/main/java/com/coralogix/calculator/CalculatorApplication.java
+++ src/main/java/com/coralogix/calculator/CalculatorApplication.java
@@ -2,9 +2,10 @@ package com.coralogix.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CalculatorApplication {
+public class CalculatorApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		System.out.println("Testing Spring boot");
 		SpringApplication.run(CalculatorApplication.class, args);
